5dd0cd6a93969c3ebc8cf706c5c8cb27b78d1fbf
Rubyboat <rudy@rudysoliz.com>
2023-06-08 20:34:24 -0500

    Now detects water

--- src/main/java/com/rubyboat/daily/items/MeshPannerItem.java
+++ src/main/java/com/rubyboat/daily/items/MeshPannerItem.java
@@ -1,11 +1,20 @@
 package com.rubyboat.daily.items;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.loot.LootTable;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
 
 public class MeshPannerItem extends Item {
     public MeshPannerItem(Settings settings) {
@@ -14,12 +23,14 @@ public class MeshPannerItem extends Item {
 
     LootTable lootTable;
 
-
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        BlockHitResult hitResult = raycast(world, user, RaycastContext.FluidHandling.SOURCE_ONLY);
+        if(hitResult.getType() == HitResult.Type.BLOCK) {
+            Block block = world.getBlockState(hitResult.getBlockPos()).getBlock();
+            user.sendMessage(block.getName(), true);
+        }
 
-        context.getPlayer().sendMessage(context.getWorld().getBlockState(context.getBlockPos()).getBlock().getName(), true);
-        return super.useOnBlock(context);
+        return super.use(world, user, hand);
     }
-
 }
